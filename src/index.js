d58845a7b183b3b9675b12b226b32c39777e81a1
mr.bmv <mr.m.botkin@gmail.com>
2017-10-31 16:07:33 +0300

    change library in index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import readlineSync from './../readline-sync';
+import readlineSync from 'readline-sync';
 
 const userGreeting = () => {
   const actual = readlineSync.question('May I have your name? ');
